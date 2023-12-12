18f098e5dd218066527fa273fc5ddab88d0643be
Paulo Henrique <paulo24041996@gmail.com>
2023-12-11 18:52:16 -0800

    Programmer Coffee

---
+++ src/main/java/com/mycompany/programmer/Programmer.java
@@ -0,0 +1,13 @@
+package com.mycompany.programmer;
+
+public class Programmer {
+    public static void main(String[] args) {
+        boolean programmer = true;
+        if (programmer == true) {
+            System.out.println("--- PRETEND WE'RE DEAD ---");
+            System.out.println("Wake up and smell the coffee.");
+        } else {
+            System.out.println("Author: Paulo");
+        }
+    }
+}
